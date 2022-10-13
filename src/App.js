98e02cb91e413afdcc9821c122a1ce8947867315
Mi Seadi <michelesobrosa@gmail.com>
2022-10-13 15:33:07 -0300

    Migra HTML dos componentes restantes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Home from "./components/pages/Home";
+import UserBlog from "./components/pages/UserBlog";
+import Users from "./components/pages/Users";
+import UserPostForm from "./components/pages/UserPostForm";
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
 import "./styles/main.css";
