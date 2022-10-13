23454ffe909577f1686c29a27489487485ff86b2
Mi Seadi <michelesobrosa@gmail.com>
2022-10-13 14:36:15 -0300

    Migra html do componente da página inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./components/pages/Home";
+import "./styles/normalize.css";
+import "./styles/fontawesome.min.css";
+import "./styles/main.css";
+
 function App() {
   return (
-   <div>
-    Projeto
-   </div>
-  );
+   <Home/>);
 }
 
 export default App;
